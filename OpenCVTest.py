205aad64ec486aea28a2560208d93612f25391f5
Drewpew <agcummings11@gmail.com>
2018-06-11 14:15:33 -0700

    working single image tail contour

---
+++ OpenCVTest.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2 as cv
+import wholeBrain as wb
+
+clicks = 0
+lightest = 0
+darkest = 255
+
+def mouse_click(event,x,y,flags,param):
+	global lightest, darkest, clicks
+	if event == cv.EVENT_LBUTTONUP:
+		print(img.item(y,x))
+		if lightest < img.item(y,x):
+			lightest = img.item(y,x)
+		if darkest > img.item(y,x):
+			darkest = img.item(y,x)
+
+		clicks += 1
+		if clicks >= 6:
+			print(darkest, lightest)
+			retval, result = cv.threshold(img, lightest+10, 255, cv.THRESH_TOZERO_INV);
+			retval, result = cv.threshold(result, darkest-10, 255, cv.THRESH_BINARY);
+
+			blah, contours, hierarchy = cv.findContours(result, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+			index = 0
+			for i, contour in enumerate(contours):
+				if cv.pointPolygonTest(contour, (x,y), False) > 0:
+					print("found it!")
+					index = i
+					break;
+
+			cv.drawContours(contors_img, contours, index, (255,255,255), 2)
+			cv.imshow("Contours", contors_img)
+
+img = cv.imread("realmouse.png", 0)
+contors_img = img.copy()
+
+cv.namedWindow("Original")
+cv.setMouseCallback("Original", mouse_click)
+
+wb.playMovie(img, )
+
+cv.imshow("Original", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
