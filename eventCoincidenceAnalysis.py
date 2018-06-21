6eb12c3804a1c2e2c94461c384660a1e4e1bbd6e
Ashwin Rammohan <19ashwinra@students.harker.org>
2018-06-21 10:08:55 -0700

    Spike detection with all the new avg, stdev stuff

--- eventCoincidenceAnalysis.py
+++ eventCoincidenceAnalysis.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+from hdf5manager import *
 
 #Method for detecting spikes in data given certain thresholds
 #data is of the form: ((xvals), (yvals))
 #delta_threshold - threshold for delta between current value and moving average
 #stDev_threshold - if a y-value is greater than the mean by this threshold * stDev, that point is declared a spike
-def detectSpike(data, interval = 10, stDev_threshold = 1.5):
+def detectSpike(data, interval = 20, stDev_threshold = 1.5):
 	xvals = data[0]
 	yvals = data[1]
 	spikeIndices = set()
+	localAvgs = []
+	localSTDs = []
 	
 	mid_interval = int(interval/2)
 
@@ -44,20 +47,50 @@ def detectSpike(data, interval = 10, stDev_threshold = 1.5):
 	localSTDev = 0
 	localAVG = 0
 	for i in range(0,len(xvals)):
+
 		before = i-mid_interval
 		after = i+mid_interval
 
-		subset = yvals[before:after]
-		localAVG = np.mean(subset)
-		localSTDev = np.std(subset)
+		std_subset = new_yvals[before:i]
+		mean_subset = new_yvals[before:i]
+		localAVG = np.mean(mean_subset)
+		localAvgs.append(localAVG)
+		localSTDev = np.std(std_subset)
+		localSTDs.append(localSTDev)
 
-		if (yvals[i] - localAVG > localSTDev * stDev_threshold):
-			print("Spike detected at x = " + str(xvals[i]))
-			print("\n" + "Index: " + str(i) + " y value: " + str(yvals[i]) + ", local mean: " + str(localAVG) + ", local std: " + str(localSTDev) + "\n")
+		if (new_yvals[i] - localAVG > localSTDev * stDev_threshold):
+			#print("Spike detected at x = " + str(xvals[i]))
+			#print("\n" + "Index: " + str(i) + " y value: " + str(yvals[i]) + ", local mean: " + str(localAVG) + ", local std: " + str(localSTDev) + "\n")
 			spikeIndices.add(i)
 
-	return spikeIndices
+	return (spikeIndices, localAvgs, localSTDs)
 
+data = hdf5manager("P2_timecourses.hdf5").load()
+print("brain data below...")
+ys = data['brain'][0][:2000]
+print("Global Average: " + str(np.mean(ys)))
+print("Global Stdev: " + str(np.std(ys)))
+
+xs = list(np.linspace(0,2000,2000))
+
+plot_data = [xs,ys]
+spikes, avgs, stdevs = detectSpike(plot_data,100,3)
+
+legend = ("Data", "Avgs", "Stdevs")
+plt.plot(xs,ys)
+plt.plot(xs,avgs)
+plt.plot(xs, stdevs)
+plt.legend(legend)
+
+
+for i in spikes:
+	#print("(" + str(xs[i]) + "," + str(ys[i]) + ")" + "\n")
+	plt.axvline(x = xs[i], color = 'red')
+
+
+plt.show()
+
+'''
 xs = [1,2,3,4,5,6,7,8,9,10,11,12]
 ys = [1,4,9,16,25,36,49,64,81,100,121,144]
 
@@ -70,6 +103,7 @@ for i in spikes:
 
 plt.scatter(xs, ys)
 plt.show()
+'''
 
 
 
